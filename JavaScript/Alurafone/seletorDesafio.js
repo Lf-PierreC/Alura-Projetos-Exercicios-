f70ae29ba7ab48322a46ec90dd9bd80d71bdfc8c
Lf-PierreC <128261200+Lf-PierreC@users.noreply.github.com>
2024-06-23 01:12:15 -0300

    Add files via upload

--- JavaScript/Alurafone/seletorDesafio.js
+++ JavaScript/Alurafone/seletorDesafio.js
@@ -1,3 +1,38 @@
-function digitar() {
+/*function digitar() {
+    let num = document.querySelectorAll('.teclado button')
+    let bina = document.getElementById('input[type="tel"')
     
+    num.forEach(button => {
+        button.addEventListener('click', () =>{
+            bina.value += button.textContent
+        })
+    })
+
+}
+
+// input[type="button"][value="' + value +'"] [value="&{value}"]
+
+function digitar(value) {
+    let num = document.querySelectorAll('.teclado button')
+    let bina = document.getElementById('place')
+
+    for (let i=0; i < 5; i++){
+    let buttonText = button.textContent
+    
+    bina.value = num[i].textContent
+    }
+}
+*/
+
+function digitar(value) {
+    let bina = document.getElementById('place')
+    bina.value += value 
+}
+
+function apagar() {
+    let bina = document.getElementById('place')
+    let currentValue = bina.value;
+    if (currentValue.length > 0) {
+        bina.value = currentValue.slice(0, -1);
+    }
 }
